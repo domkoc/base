a4f26fd4c1eee59a5691608fdf43dff5c16f1ef4
Dominik Kocka <kocka98@yahoo.com>
2021-03-01 15:54:57 +0100

    added guava

---
+++ train-tachograph/src/main/java/hu/bme/mit/train/tachograph/TrainTachographImpl.java
@@ -0,0 +1,9 @@
+package hu.bme.mit.train.tachograph;
+import com.google.guava;
+
+public class TrainTachographImpl {
+
+	private Table<Integer, Integer, Integer> universityCourseSeatTable
+			= HashBasedTable.create();
+
+}
