9571b0004c7ec27c8dc1d27322f28b80a5c2cba4
Dominik Kocka <kocka98@yahoo.com>
2021-03-17 16:53:20 +0100

    added JUnit tests to test the sensor and user alarm function

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -1,5 +1,8 @@
 package hu.bme.mit.train.sensor;
 
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.interfaces.TrainUser;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,13 +10,44 @@ import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
 
+    TrainUser mockUser;
+    TrainController mockController;
+    TrainSensor sensor;
+
     @Before
     public void before() {
-        // TODO Add initializations
+        mockUser = mock(TrainUser.class);
+        mockController = mock(TrainController.class);
+        sensor = new TrainSensorImpl(mockController, mockUser);
+        when(mockController.getReferenceSpeed()).thenReturn(200);
     }
 
     @Test
-    public void ThisIsAnExampleTestStub() {
-        // TODO Delete this and add test cases based on the issues
+    public void OverridingSpeedLimitToMoreThanFiveHundred_CallsSetAlarmStateWithTrueOnce() {
+        sensor.overrideSpeedLimit(2000);
+
+        verify(mockUser, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void OverridingSpeedLimitToMinus_CallsSetAlarmStateWithTrueOnce() {
+        sensor.overrideSpeedLimit(-1);
+
+        verify(mockUser, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void OverridingSpeedLimitToLessThanHalf_CallsSetAlarmStateWithTrueOnce() {
+        sensor.overrideSpeedLimit(1);
+
+        verify(mockUser, times(1)).setAlarmState(true);
+    }
+
+    @Test
+    public void OverridingSpeedLimitToANormalValue_DoesNotCallSetAlarmStateTrue() {
+        sensor.overrideSpeedLimit(250);
+
+        verify(mockUser, times(0)).setAlarmState(true);
+        verify(mockUser, times(0)).setAlarmState(false);
     }
 }
